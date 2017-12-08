b425bc4d6fb60367e1645319503e0ad4dc3e2a08
arunkumar4all <arunkumar@global.t-bird.edu>
2017-12-08 08:36:03 -0600

    latest commit 12082017

--- unobankproject/R/unobankproject.R
+++ unobankproject/R/unobankproject.R
@@ -1,34 +1,20 @@
-#' @title bin_name
-#' @description Create bin's in increments
-#' @details Calculate the percentage of concordant and discordant pairs for a given logit model.
+
+
+
+#' @title pct_freq
+#' @description Create percentage table and shows the result in a markdown format
+#' @details This function takes a vector as input and creates their frequency and distribution. 
+#' There are two columns output from this pct function. First column "count" shows number of occurrences of a string or a number
+#' Second variable "Percentage" shows the percentage frequency of this number.
 #' @author Arun Ranganathan \email{aranganathan@unomaha.edu}
-#' @export bin_name
-#' @param x Number
-#' @return Bins
-#' @examples
-#' bin_name(100)
-
-
-bin_name = function(x) {
-  fico_increments <- 40
-  low = 480 + (x - 1) * fico_increments
-  high = low + fico_increments
-  paste(low, high, sep = "-")
-}
-
-
-#' @title pct
-#' @description Create pct's in a table
-#' @details Calculate percentage/frequency.
-#' @author Arun Ranganathan \email{aranganathan@unomaha.edu}
-#' @export pct
-#' @param x Number
-#' @return table
+#' @export pct_freq
+#' @param x is a vector of numbers or characters. This could take a column as well as a vector.
+#' @return table in a kable format using knitr package
 #' @examples
 #' a<-rbind(sample(c(1:10),20, replace = T))
 #' pct(a)
 
-pct <- function(x) {
+pct_freq <- function(x) {
   tbl <- table(x)
   tbl_pct <- cbind(tbl, round(prop.table(tbl) * 100, 2))
   colnames(tbl_pct) <- c('Count', 'Percentage')
